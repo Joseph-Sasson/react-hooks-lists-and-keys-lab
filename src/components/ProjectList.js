1efc68429362c8b348ab1000880aac6430f3d6b4
Joseph <josephsasson1@gmail.com>
2021-06-30 09:09:05 -0400

    commit tree fc009953c1daf6dea00851d05adf938d10fb8835

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,18 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectItem = projects.map((item)=>{
+    return (
+    <div key={item.id}>
+      <h3>{item.name}</h3>
+      <p>{item.about}</p>
+      <nav>{item.technologies}</nav>
+    </div>)
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectItem}</div>
     </div>
   );
 }
